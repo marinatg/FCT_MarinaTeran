86b67300bcb154f93ccafc6e5f306677d2ab270d
Marina <marinatg93@gmail.com>
2023-05-03 18:18:07 +0200

    registro, login, logout

--- main/views.py
+++ main/views.py
@@ -1,11 +1,66 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import *
+from django.db import transaction
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView, ListView
 
 from main.models import *
 
 
 # Create your views here.
 
+"""REGISTRO, LOGIN Y LOGOUT"""
+class Registro(CreateView):
+    template_name = 'main/registro.html'
+    form_class = UserCreationForm
+    def get_success_url(self):
+        return reverse('welcome')
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+
+            if request.POST['password1'] == request.POST['password2']:
+                try:
+                    user = User.objects.create_user(username=request.POST['username'],
+                                                    password=request.POST['password1'])
+                    user.save()
+
+                    perfil = Perfil.objects.create(usuario_id=user.id,
+                                    dni=request.POST['dni'],
+                                    telefono=request.POST['telefono'])
+
+                    perfil.save()
+
+                    login(request, user)
+                    return redirect('welcome')
+                except:
+                    return render(request, "main/registro.html",
+                                  {'form': UserCreationForm, 'error': 'El usuario ya existe'})
+            return render(request, "main/registro.html",
+                          {'form': UserCreationForm, 'error': 'Las contraseñas no coinciden'})
+
+class CerrarSesion(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('welcome')
+
+class IniciarSesion(View):
+    def get(self, request, *args, **kwargs):
+        form = AuthenticationForm()
+        return render(request, 'main/iniciarSesion.html', {'form': AuthenticationForm})
+
+    def post(self, request, *args, **kwargs):
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'main/iniciarSesion.html', {'form': AuthenticationForm, 'error': 'El usuario o la contraseña son incorrectos'})
+        else:
+            login(request, user)
+            return redirect('welcome')
+
+"""PERFIL DE USUARIO"""
 class PerfilCliente(TemplateView):
     template_name = 'main/perfil.html'
     model= Perfil
@@ -17,3 +72,45 @@ class PerfilCliente(TemplateView):
         "pago = Metodo_pago.objects.filter(perfil_id=perfil[0].id)"
 
         return render(request, self.template_name, {'perfil': perfil})
+
+class AgregarPerfil(CreateView):
+    model = Perfil
+    template_name = 'main/agregarPerfil.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('perfil')
+
+class EditarPerfil(UpdateView):
+    model = Perfil
+    fields = '__all__'
+
+    def get_success_url(self, **kwargs):
+        return reverse('perfil')
+
+class EliminarPerfil(DeleteView):
+    model = Perfil
+    fields = '__all__'
+    def get_success_url(self, *args, **kwargs):
+        return reverse('perfil')
+
+class AgregarMetodoPago(CreateView):
+    model = Metodo_pago
+    template_name = 'main/agregarMetodoPago.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('perfil')
+
+class EditarMetodoPago(UpdateView):
+    model = Metodo_pago
+    fields = '__all__'
+
+    def get_success_url(self, **kwargs):
+        return reverse('perfil')
+
+class EliminarMetodoPago(DeleteView):
+    model = Metodo_pago
+    fields = '__all__'
+    def get_success_url(self, *args, **kwargs):
+        return reverse('perfil')
